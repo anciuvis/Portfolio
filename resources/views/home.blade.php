a257afc00831ec7bad4ccfb6f88f0022aee488e5
Anna Yasyreva <nonofthem@gmail.com>
2018-03-27 19:12:33 +0300

    flexbox isssue solved

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
 		<div class="wrapper">
 			<div class="content-wrapper p-3">
 				<section class="cubes row m-0">
-					<div class="col-xs-12 col-sm-6 col-md-4 p-0 cube homecube d-flex align-items-center justify-content-center">
+					<div class="col-xs-12 col-sm-6 col-md-4 cube homecube d-flex align-items-center justify-content-center p-3">
 						<a href="/">The portfolio</a>
 					</div>
-					<div class="col-xs-12 col-sm-6 col-md-4 p-0 cube d-flex align-items-center justify-content-center">
+					<div class="col-xs-12 col-sm-6 col-md-4 cube d-flex align-items-center justify-content-center p-3">
 						<a href="/">
 							<i class="fas fa-user-circle fa-2x"></i>
 							About me
@@ -16,11 +16,11 @@
 					</div>
 					<div class="col-sm-6 col-md-4 p-0 cube colorcube d-none d-sm-block">
 					</div>
-					<div class="col-xs-12 col-sm-6 col-md-4 p-0 cube introcube d-flex align-items-center text-left p-2">
+					<div class="col-xs-12 col-sm-6 col-md-4 cube introcube d-flex align-items-center text-left p-3">
 						Hello! <br />
 						I am Anna Yasyreva and this is my portfolio website
 					</div>
-					<div class="col-xs-12 col-sm-6 col-md-4 p-0 cube photocube d-flex align-items-center justify-content-center">
+					<div class="col-xs-12 col-sm-6 col-md-4 p-0 cube photocube d-flex align-items-center justify-content-center p-3">
 						<a href="/">
 							<i class="fas fa-camera-retro fa-2x"></i>
 							My Photos
@@ -31,17 +31,22 @@
 					</div>
 					<div class="col-md-4 p-0 cube colorcube d-none d-md-block">
 					</div>
-					<div class="col-xs-12 col-sm-6 col-md-4 p-0 cube contactcube d-flex align-items-center justify-content-center">
-						<i class="fas fa-paper-plane fa-2x"></i>
+					<div class="col-xs-12 col-sm-6 col-md-4 p-0 cube contactcube d-flex align-items-center justify-content-center p-3">
+						<div class="">
+							<i class="fas fa-paper-plane fa-2x"></i>
+						</div>
+
 						Contacts
 					</div>
-					<div class="col-xs-12 col-sm-6 col-md-4 p-0 cube d-flex flex-column align-items-center justify-content-center">
+					<div class="col-xs-12 col-sm-6 col-md-4 p-0 cube d-flex flex-column justify-content-center p-3">
 						<div>Social networks</div>
 						<div class="d-flex justify-content-between flex-row">
-							<i class="fab fa-facebook-square fa-2x"></i>
-							<i class="fab fa-github-alt fa-2x"></i>
-							<i class="fab fa-instagram fa-2x"></i>
-							<i class="fab fa-linkedin fa-2x"></i>
+							<a href="https://www.facebook.com/anchiukas">
+								<i class="fab fa-facebook-square fa-2x icon"></i>
+							</a>
+							<i class="fab fa-github-alt fa-2x icon"></i>
+							<i class="fab fa-instagram fa-2x icon"></i>
+							<i class="fab fa-linkedin fa-2x icon"></i>
 						</div>
 					</div>
 				</section>
