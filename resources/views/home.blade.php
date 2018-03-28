1be36e583b9a1ee35a7b0ebbc0c83b3e8c4ac74a
Anna Yasyreva <nonofthem@gmail.com>
2018-03-28 21:06:55 +0300

    homepage style finish +favicon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,11 @@
 					<div class="col-xs-12 col-sm-6 col-md-4 cube homecube d-flex align-items-center justify-content-center p-3">
 						<a href="/">The portfolio</a>
 					</div>
-					<div class="col-xs-12 col-sm-6 col-md-4 cube d-flex align-items-center justify-content-center p-3">
-						<a href="/" class="hvr-back-pulse">
+					<div class="col-xs-12 col-sm-6 col-md-4 cube aboutcube d-flex align-items-center justify-content-center p-3">
+						<a href="/" class="">
+							<i class="fas fa-user-circle fa-2x p-3 scale hvr-back-pulse"></i>
+							<br />
 							ABOUT ME
-							<div class="">
-								<i class="fas fa-user-circle fa-2x hvr-back-pulse"></i>
-							</div>
 						</a>
 					</div>
 					<div class="col-sm-6 col-md-4 p-0 cube colorcube d-none d-sm-block">
@@ -23,11 +22,10 @@
 						I am Anna Yasyreva and this is my portfolio website
 					</div>
 					<div class="col-xs-12 col-sm-6 col-md-4 p-0 cube photocube d-flex align-items-center justify-content-center p-3">
-						<a href="/">
-							<div class="">
-								<i class="fas fa-camera-retro fa-2x hvr-buzz-out"></i>
-							</div>
-							My Photos
+						<a href="/" class="d-flex flex-column justify-content-between">
+							<i class="fas fa-camera-retro fa-2x hvr-buzz-out p-3"></i>
+							<br />
+								My Photos
 						</a>
 					</div>
 					<div class="col-sm-6 col-md-4 p-0 cube d-none d-sm-block">
@@ -35,17 +33,16 @@
 					</div>
 					<div class="col-md-4 p-0 cube colorcube d-none d-md-block">
 					</div>
-					<div class="col-xs-12 col-sm-6 col-md-4 p-0 cube contactcube d-flex align-items-center justify-content-around p-3">
-						<a href="/">
-							<div class="">
-								<i class="fas fa-paper-plane fa-2x hvr-bob"></i>
-							</div>
+					<div class="col-xs-12 col-sm-6 col-md-4 cube contactcube d-flex flex-column justify-content-around p-3">
+						<a href="/" class="d-flex flex-column justify-content-between">
+							<i class="fas fa-paper-plane fa-2x hvr-bob p-3"></i>
+							<br />
 							Contacts
 						</a>
 					</div>
-					<div class="col-xs-12 col-sm-6 col-md-4 p-0 cube socialcube d-flex flex-column justify-content-around p-3">
-						<div>Social networks</div>
-						<div class="d-flex justify-content-between flex-row">
+					<div class="col-xs-12 col-sm-6 col-md-4 cube socialcube d-flex flex-column justify-content-around p-3">
+						Social networks
+						<div class="d-flex justify-content-between flex-row pb-3">
 							<a href="https://www.facebook.com/anchiukas">
 								<i class="fab fa-facebook-square fa-2x hvr-wobble-horizontal"></i>
 							</a>
@@ -63,4 +60,8 @@
 				</section>
 			</div>
 		</div>
+		<!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+		<!-- to generate textured pattern canvas -->
+		<script src="{{ asset('js/patternizer.js') }}"></script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Anna portfolio') }}</title>
-
     <!-- Styles -->
 		<link href="https://fonts.googleapis.com/css?family=Cinzel+Decorative" rel="stylesheet">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="anonymous">
@@ -17,13 +14,6 @@
 		<link href="{{ asset('css/styles.css') }}" rel="stylesheet">
 </head>
 	<body>
-
     @yield('content')
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-		<!-- to generate textured pattern canvas -->
-		<script src="{{ asset('js/patternizer.js') }}"></script>
-		<!-- GRADIENT animation -->
-		<script src="{{ asset('js/gradient.js') }}"></script>
 	</body>
 </html>
